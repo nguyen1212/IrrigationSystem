604d625282df336265928a14b366a34f185a0363
Pod Nguyen <nguyen.tran.pod@gmail.com>
2021-05-20 16:33:03 +0700

    applying websocket

--- Frontend/src/components/panel/panel.vue
+++ Frontend/src/components/panel/panel.vue
@@ -4,7 +4,7 @@
         <span style="float:left; color: white;">{{currentDateTime()}}</span>
         <span style="float:right; color: white;">
           <p> User Name
-          <b-button style="float:right;" size="sm" @click="logout"><b-icon icon="power" aria-hidden="true"></b-icon>Logout</b-button>
+          <b-button style="float:right;" size="sm" @click="send"><b-icon icon="power" aria-hidden="true"></b-icon>Logout</b-button>
           </p>
         </span>
     </mdb-card-title>
@@ -24,6 +24,20 @@
       mdbCardTitle,
       slides
     },
+    data(){ return{
+      ws:null
+    }},
+    created: function(){
+      // var self = this;
+      console.log("Starting connection to WebSocket Server")
+      this.ws = new WebSocket('ws://' + window.location.host + '/devices/ws');
+      // this.ws.addEventListener()
+      this.ws.onopen = function(event) {
+        console.log(event)
+        console.log("Successfully connected to the echo websocket server...")
+      }
+
+    },
     methods: {
     currentDateTime() {
       const current = new Date();
@@ -33,17 +47,26 @@
       return 'Date: ' + date;
     },
     logout() {
-            firebase
-                .auth()
-                .signOut()
-                .then(() => {
-                    this.$router.push('/');
-                })
-                .catch(error => {
-                    alert(error.message);
-                    this.$router.push('/');
-                });
-        },
+      firebase
+          .auth()
+          .signOut()
+          .then(() => {
+              this.$router.push('/');
+          })
+          .catch(error => {
+              alert(error.message);
+              this.$router.push('/');
+          });
+    },
+    send: function(){
+      this.ws.send(
+        JSON.stringify({
+          userId: 'admin@gmail.com',
+          plotId: 'california',
+          state: 'On'
+        })
+      )
+    }
   }
   }
 </script>
