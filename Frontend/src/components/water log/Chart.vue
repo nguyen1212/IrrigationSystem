c2b093078734260445680b4e97a569e33cfe0af7
MINH Vu Le Thien <vlthminh@gmail.com>
2021-06-09 22:23:31 +0700

    Frontend WaterLog

---
+++ Frontend/src/components/water log/Chart.vue
@@ -0,0 +1,21 @@
+<script>
+import { Bar } from 'vue-chart.js'
+
+export default {
+  extends: Bar,
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+
+</script>
