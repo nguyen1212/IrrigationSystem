83954fb069b3735108357ee8951026bf5d6d3451
Anh Khoa Do <dakhoa0903@gmail.com>
2021-06-17 02:31:35 +0700

    Auto mode: remove card view, add handler for preset add, update, delete, select

--- Frontend/src/components/auto mode/AddModal.vue
+++ Frontend/src/components/auto mode/AddModal.vue
@@ -1,69 +1,36 @@
 <template>
  <div>
-  <!-- <b-button id="show-btn" @click="$bvModal.show(myid)">Open Modal</b-button> -->
-
-  <b-modal 
-  :id="myid" 
-  button-size="md"
-  @show="resetModal"
-  @hidden="resetModal"
-  @ok="handleSubmit">
-    <template #modal-title>
-      Add new preset!
-    </template>
-    <div class="d-block text-center">
-    <b-form v-if="show">
-      <b-form-group id="input-group-2" label="Preset Name" label-for="input-2">
+<b-modal v-model="showModal" :ref="customref" title="Add new preset" hide-footer>
+    <div>
+    <b-form 
+    @submit="handleAddPreset" 
+    @reset="resetForm">
+      <b-form-group id="input-group-1" label="Preset Name" label-for="input-1">
         <b-form-input
-          id="input-2"
+          id="input-1"
           v-model="form.name"
           placeholder="Enter name"
           required
         ></b-form-input>
       </b-form-group>
-
-      <b-form-group id="input-group-2" label="Humidity" label-for="input-2">
-
-        <b-form-spinbutton 
-        id="demo-sb" 
-        v-model="form.humidity"
-        min="1" 
-        max="100"></b-form-spinbutton>
-      </b-form-group>
-
       <b-form-group id="input-group-2" label="Moisture" label-for="input-2">
         <b-form-spinbutton 
-        id="demo-sb" 
+        id="input-2" 
         v-model="form.moisture"
         min="1" 
         max="100"></b-form-spinbutton>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Temperature" label-for="input-2">
-        <b-form-spinbutton 
-        id="demo-sb" 
-        v-model="form.temperature"
-        min="1" 
-        max="100"></b-form-spinbutton>
-      </b-form-group>
-        <b-form-group id="input-group-2" label="Notes" label-for="input-2">
+        <b-form-group id="input-group-3" label="Notes" label-for="input-3">
         <b-form-textarea
-        id="input-2"
-          v-model="form.name"
+        id="input-3"
+          v-model="form.note"
         placeholder="Default textarea"
       ></b-form-textarea>
       </b-form-group>
-    </b-form>
-    </div>
-    <template #modal-footer="{ ok, cancel }">
-      <!-- Emulate built in modal footer ok and cancel button actions -->
-      <b-button variant="success" @click="ok()">
-        Submit
-      </b-button>
-      <b-button variant="danger" @click="cancel()">
-        Cancel
-      </b-button>
-    </template>
+      <b-button type="submit" variant="primary" class="mr-3">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form></div>
   </b-modal>
 </div>
 </template>
@@ -75,16 +42,14 @@ export default {
       return {
         form: {
           name: '',
-          humidity: 0,
           moisture: 0,
-          temperature: 0,
           notes: ""
         },
         showModal: false,
       }
     },
     props: {
-      myid: String
+      customref: String
     },
     methods: {
       show() {
@@ -99,18 +64,13 @@ export default {
       resetForm()
       {
         this.form.name=""
-        this.form.humidity=0
         this.form.moisture=0
-        this.form.temperature=0
         this.form.notes=""
-        // this.show = false
-        // this.$nextTick(() => {
-        //   this.show = true
-        // })
       },
       handleSubmit()
       {
-        
+        console.log(this.form)
+        this.$emit('add-preset', this.form)
       }
     }
 }
